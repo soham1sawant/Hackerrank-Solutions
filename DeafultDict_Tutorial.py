f4c4560af61587e2b4bb349b550bd1aac7c973d6
Soham Sawant <geekysoham@gmail.com>
2018-10-25 22:48:52 +0530

    Added new solutions

--- DeafultDict_Tutorial.py
+++ DeafultDict_Tutorial.py
@@ -1,1 +1,34 @@
 # DeafultDict_Tutorial.py
+
+def compute():
+    n , m = map(int , input("Enter space separated values of 'n' and 'm' : ").split())
+    list = []
+    for i in range(n):
+        ele = input("Enter the word/character : ")
+        list.append(ele)
+
+    final_ans = []
+    for i in range(m):
+        ele = input("Enter the word/character : ")
+        ans = ''
+        for j in range(len(list)):
+            if list[j] == ele:
+                ans += str(j+1)+' '
+        ans = ans.strip()
+        if ans == '':
+            final_ans.append(-1)
+        else:
+            final_ans.append(ans)
+
+    for i in final_ans:
+        print(i)
+
+
+def main():
+    print()
+    print("Hackerrank : Defaultdict Tutorial")
+    print()
+
+    compute()
+
+main()
