747c8e3e12ebcbf92f0e262d0932868d6ba50e99
Soham Sawant <geekysoham@gmail.com>
2019-11-07 20:27:30 +0530

    Added new solutions

---
+++ alphabet_rangoli.py
@@ -0,0 +1,14 @@
+# alphabet_rangoli.py
+
+def print_rangoli(size):
+    print(size)
+
+def main():
+    print()
+    print("Hackerrank : Alphabet Rangoli")
+    print()
+
+    n = int(input("Enter the value of 'N' : "))
+    print_rangoli(n)
+
+main()
--- designer_door_mat.py
+++ designer_door_mat.py
@@ -2,24 +2,23 @@
 
 def compute():
     n,m = map(int,input().split())
-    oddnum = []                                     
+    oddnum = []                               # creates list for all odd numbers till 'n'      
     for i in range(1,n+1,2):                
         oddnum.append(i)
-    center =  round(n/2)
+    center =  int(n/2)              #takes the center of 'n' and converts it into int
 
-    oddnum1 = []
-    for i in range(0,center-1):
+    oddnum1 = []                # creates list for collecting oddnumbers from oddnum till center
+    for i in range(center):
         oddnum1.append(oddnum[i])
-    print(oddnum1)
 
-    #Top Part
-    for i in oddnum1:
+    #Top Part                   
+    for i in oddnum1:                   # runs through oddnum1 and print the top half with center()
         print((".|."*i).center(m,'-'))
-
-    #Center Part
+    
+    #Center Part                        # prints the 'WELCOME' message at the center
     print("WELCOME".center(m,'-'))
 
-    # Bottom Part
+    # Bottom Part                       # same as the top part but runs through oddnum1 in reverse order with [::-1]
     for i in oddnum1[::-1]:
         print((".|."*i).center(m,'-'))
 
