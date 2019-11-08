aa632199c0337b3670055278da1855b5ddd969fd
Soham Sawant <geekysoham@gmail.com>
2019-11-08 21:16:23 +0530

    Added new solutions

--- alphabet_rangoli.py
+++ alphabet_rangoli.py
@@ -1,7 +1,14 @@
 # alphabet_rangoli.py
 
+from string import ascii_lowercase
+
 def print_rangoli(size):
-    print(size)
+    mystr = ascii_lowercase[0:size]         # takes the required string from the aplhabet
+    
+    for i in range(size-1 , -size , -1):
+        x = abs(i)
+        line = mystr[size:x:-1] + mystr[x:size]
+        print('--'*x + '-'.join(line) + '--'*x)
 
 def main():
     print()
---
+++ the_minion_game.py
@@ -0,0 +1,8 @@
+# the_minion_game.py
+
+def minion_game(string):
+    print(string)
+
+if __name__ == '__main__':
+    s = input().upper()
+    minion_game(s)
