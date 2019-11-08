96c33242e675d024e0ae51f016835970047e9f40
Soham Sawant <geekysoham@gmail.com>
2019-11-08 23:31:20 +0530

    Added new solutions

--- the_minion_game.py
+++ the_minion_game.py
@@ -1,7 +1,15 @@
 # the_minion_game.py
 
 def minion_game(string):
-    print(string)
+    string = list(string)
+    stuart_conso = 0
+    kevin_vow = 0
+    vowels = 'AEOIU'
+    
+    stuart_str = []
+    for i in range(len(string)):
+        if string[i] in vowels:
+            substr = []
 
 if __name__ == '__main__':
     s = input().upper()
