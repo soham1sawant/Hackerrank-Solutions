24e8150c123b0027a1bbf431a4a35462d254c5d9
Soham Sawant <geekysoham@gmail.com>
2019-12-06 21:28:19 +0530

    Added new solutions

--- utopian_tree.py
+++ utopian_tree.py
@@ -4,9 +4,9 @@ def utopianTree(n):
     height = 0
     for i in range(n):
         if i%2 == 0:
-            height += 1
-        else:
             height  = height * 2
+        else:
+            height += 1
     
     return height
 
