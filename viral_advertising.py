e32e89c596a9f49de33fa047388e2bbc906c86f6
Soham Sawant <geekysoham@gmail.com>
2019-12-16 21:15:18 +0530

    Added new solutions

--- viral_advertising.py
+++ viral_advertising.py
@@ -1,7 +1,14 @@
 # viral_advertising.py
 
 def viralAdvertising(n):
+    total = 2
+    liked = 2
+
+    for _ in range(n-1):
+        liked = liked * 3//2
+        total += liked
     
+    return total
 
 
 if __name__ == '__main__':
