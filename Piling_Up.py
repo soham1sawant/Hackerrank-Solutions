a01faf7d9c9716376202e7648a00b38bac00ec9f
Soham Sawant <geekysoham@gmail.com>
2020-06-16 22:21:17 +0530

    added new solutions

--- Piling_Up.py
+++ Piling_Up.py
@@ -9,6 +9,31 @@ def compute():
 
         stack = []
         check = True
-        while (data != []) or check:
+        while (len(data) == 0) and check:
+            left = data[0]
+            right = data[len(data) - 1]
+
             if stack != []:
-                
+                if right > left and right <= stack[len(stack) - 1]:
+                    stack.append(data.pop())
+                elif left > right and left <= stack[len(stack) - 1]:
+                    stack.append(data.popleft())
+                elif left == right and left <= stack[len(stack) - 1]:
+                    stack.append(data.pop())
+                else:
+                    check = False
+            else:
+                if right > left:
+                    stack.append(data.pop())
+                elif left > right:
+                    stack.append(data.popleft())
+                else:
+                    stack.append(data.pop())
+
+        if check:
+            print("Yes")
+        else:
+            print("No")
+
+if __name__ == "__main__":
+    compute()
